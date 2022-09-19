4d6735fb43bd47cda839024b33447575fd85330a
Rohit Balage <thatcomputerboy26@gmail.com>
2022-09-18 23:43:35 -0400

    Initial Commit with Bottom Navigation bar

---
+++ android/app/src/main/kotlin/com/rrbofficial/multi_store_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rrbofficial.multi_store_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_screens/customer_home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CustomerHomeScreen extends StatefulWidget {
+  const CustomerHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CustomerHomeScreen> createState() => _CustomerHomeScreenState();
+}
+
+class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _tabs = const [
+    Center(child: Text('home screen'),),
+    Center(child: Text('category screen'),),
+    Center(child: Text('stores screen'),),
+    Center(child: Text('cart screen'),),
+    Center(child: Text('profile screen'),),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor:Colors.black,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        currentIndex:_selectedIndex,
+        items: [BottomNavigationBarItem(icon:Icon(Icons.home), label: 'Home',),
+          BottomNavigationBarItem(icon:Icon(Icons.search), label: 'Category',),
+          BottomNavigationBarItem(icon:Icon(Icons.shop), label: 'Stores',),
+          BottomNavigationBarItem(icon:Icon(Icons.shopping_cart), label: 'Cart',),
+          BottomNavigationBarItem(icon:Icon(Icons.person), label: 'Profile',),
+
+        ],
+        onTap:(index)
+        {
+          setState(() {
+            _selectedIndex=index;
+          });
+
+        } ,
+      ),
+
+
+
+    );
+  }
+}
