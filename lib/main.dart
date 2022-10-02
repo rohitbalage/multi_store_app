d20eebad2b79013f37c0bf9565d3cd729145aa5b
Rohit Balage <thatcomputerboy26@gmail.com>
2022-10-01 23:23:35 -0400

    Supplier Screen Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:multi_store_app/main_screens/customer_home.dart';
+import 'package:multi_store_app/main_screens/supplier_home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-    debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       title: 'Multi Store App',
-      home: CustomerHomeScreen(),
+      home: SupplierHomeScren(),
     );
   }
 }
---
+++ lib/main_screens/supplier_home.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:multi_store_app/main_screens/category.dart';
+import 'package:multi_store_app/main_screens/home.dart';
+import 'package:multi_store_app/main_screens/stores.dart';
+
+class SupplierHomeScren extends StatefulWidget {
+  const SupplierHomeScren({Key? key}) : super(key: key);
+
+  @override
+  State<SupplierHomeScren> createState() => _CustomerHomeScreenState();
+}
+
+class _SupplierHomeScreenState extends State<SupplierHomeScren> {
+  int _selectedIndex = 0;
+  final List<Widget> _tabs = const [
+    HomeScreen(),
+    CategoryScreen(),
+    StoresScreen(),
+    Center(
+      child: Text('Dashboard'),
+    ),
+    Center(
+      child: Text('upload'),
+    )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        currentIndex: _selectedIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Category',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Stores',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.upload),
+            label: 'Upload',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
