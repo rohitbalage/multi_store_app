c63fa3873202eae3a2c6d352a72bfc810ebcb01d
Rohit Balage <thatcomputerboy26@gmail.com>
2022-10-01 23:11:44 -0400

    Completed ProfileScreen  UI design

--- lib/main_screens/profile.dart
+++ lib/main_screens/profile.dart
@@ -11,151 +11,187 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(slivers: [
-      SliverAppBar(
-        centerTitle: true,
-        floating: false,
-        elevation: 0,
-        backgroundColor: Colors.white,
-        pinned: false,
-        expandedHeight: 200,
-        flexibleSpace: LayoutBuilder(builder: (context, Constraints) {
-          return FlexibleSpaceBar(
-            title: AnimatedOpacity(
-              duration: const Duration(milliseconds: 200),
-              opacity: Constraints.biggest.height <= 120 ? 1 : 0,
-              child:
-                  const Text('Account', style: TextStyle(color: Colors.black)),
-            ),
-            background: Container(
+        body: Stack(children: [
+      Container(
+        height: 230,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.yellow, Colors.brown])),
+      ),
+      CustomScrollView(slivers: [
+        SliverAppBar(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          pinned: true,
+          expandedHeight: 200,
+          flexibleSpace: LayoutBuilder(builder: (context, Constraints) {
+            return FlexibleSpaceBar(
+              title: AnimatedOpacity(
+                duration: const Duration(milliseconds: 200),
+                opacity: Constraints.biggest.height <= 120 ? 1 : 0,
+                child: const Text('Account',
+                    style: TextStyle(color: Colors.black)),
+              ),
+              background: Container(
                 decoration: const BoxDecoration(
                     gradient:
-                        LinearGradient(colors: [Colors.yellow, Colors.brown]))),
-          );
-        }),
-      ),
-      SliverToBoxAdapter(
-        child: Column(children: [
-          Container(
-            height: 80,
-            width: MediaQuery.of(context).size.width * 0.9,
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(50)),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.black54,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          bottomLeft: Radius.circular(30)),
-                    ),
-                    child: TextButton(
-                      child: const Center(
+                        LinearGradient(colors: [Colors.yellow, Colors.brown])),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 25, left: 30),
+                  child: Row(
+                    children: [
+                      const CircleAvatar(
+                          radius: 50,
+                          backgroundImage:
+                              AssetImage('images/inapp/guest.jpg')),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25),
                         child: Text(
-                          'Cart',
-                          style: TextStyle(color: Colors.yellow, fontSize: 20),
+                          'guest'.toUpperCase(),
+                          style: const TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.w600),
                         ),
-                      ),
-                      onPressed: () {},
-                    ),
+                      )
+                    ],
                   ),
-                  Container(
-                    color: Colors.yellow,
-                    child: TextButton(
-                      child: const Center(
-                        child: Text(
-                          'Orders',
-                          style: TextStyle(color: Colors.black54, fontSize: 20),
-                        ),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.black54,
-                      borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(30),
-                          bottomRight: Radius.circular(30)),
-                    ),
-                    child: TextButton(
-                      child: const Center(
-                        child: Text(
-                          'Whishlist',
-                          style: TextStyle(color: Colors.yellow, fontSize: 20),
-                        ),
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                ]),
-          ),
-          const SizedBox(
-            height: 150,
-            child: Image(image: AssetImage('images/inapp/logo.jpg')),
-          ),
-
-          //calling profileheaderlabel
-          const ProfileHeaderLabel(
-            headerLabel: 'Account info',
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              height: 260,
+                ),
+              ),
+            );
+          }),
+        ),
+        SliverToBoxAdapter(
+          child: Column(children: [
+            Container(
+              height: 80,
+              width: MediaQuery.of(context).size.width * 0.9,
               decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(16)),
-              child: Column(children: const [
-                RepeatedListTile(
-                    icon: Icons.email,
-                    subTitle: 'example@email.com',
-                    title: 'Email Address'),
-                YellowDivider(),
-                RepeatedListTile(
-                    icon: Icons.phone,
-                    subTitle: '1111111111',
-                    title: 'Phone Number'),
-                YellowDivider(),
-                RepeatedListTile(
-                    icon: Icons.location_pin,
-                    subTitle: '1801 E 12th screen',
-                    title: 'Adress'),
-              ]),
+                  color: Colors.white, borderRadius: BorderRadius.circular(50)),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            bottomLeft: Radius.circular(30)),
+                      ),
+                      child: TextButton(
+                        child: const Center(
+                          child: Text(
+                            'Cart',
+                            style:
+                                TextStyle(color: Colors.yellow, fontSize: 20),
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Container(
+                      color: Colors.yellow,
+                      child: TextButton(
+                        child: const Center(
+                          child: Text(
+                            'Orders',
+                            style:
+                                TextStyle(color: Colors.black54, fontSize: 20),
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(30),
+                            bottomRight: Radius.circular(30)),
+                      ),
+                      child: TextButton(
+                        child: const Center(
+                          child: Text(
+                            'Whishlist',
+                            style:
+                                TextStyle(color: Colors.yellow, fontSize: 20),
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ]),
             ),
-          ),
-          const ProfileHeaderLabel(headerLabel: ' Account Settings  '),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              height: 260,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(16)),
+            Container(
+              color: Colors.grey.shade300,
               child: Column(children: [
-                RepeatedListTile(
-                  title: 'Edit Profile',
-                  subTitle: '',
-                  icon: Icons.edit,
-                  onPressed: () {},
+                const SizedBox(
+                  height: 150,
+                  child: Image(image: AssetImage('images/inapp/logo.jpg')),
                 ),
-                YellowDivider(),
-                RepeatedListTile(
-                  title: 'Change Password',
-                  icon: Icons.lock,
-                  onPressed: () {},
+
+                //calling profileheaderlabel
+                const ProfileHeaderLabel(
+                  headerLabel: 'Account info',
                 ),
-                YellowDivider(),
-                RepeatedListTile(
-                  title: 'Log Out',
-                  icon: Icons.logout,
-                  onPressed: () {},
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 260,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16)),
+                    child: Column(children: const [
+                      RepeatedListTile(
+                          icon: Icons.email,
+                          subTitle: 'example@email.com',
+                          title: 'Email Address'),
+                      YellowDivider(),
+                      RepeatedListTile(
+                          icon: Icons.phone,
+                          subTitle: '1111111111',
+                          title: 'Phone Number'),
+                      YellowDivider(),
+                      RepeatedListTile(
+                          icon: Icons.location_pin,
+                          subTitle: '1801 E 12th screen',
+                          title: 'Adress'),
+                    ]),
+                  ),
+                ),
+                const ProfileHeaderLabel(headerLabel: ' Account Settings  '),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 260,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16)),
+                    child: Column(children: [
+                      RepeatedListTile(
+                        title: 'Edit Profile',
+                        subTitle: '',
+                        icon: Icons.edit,
+                        onPressed: () {},
+                      ),
+                      YellowDivider(),
+                      RepeatedListTile(
+                        title: 'Change Password',
+                        icon: Icons.lock,
+                        onPressed: () {},
+                      ),
+                      YellowDivider(),
+                      RepeatedListTile(
+                        title: 'Log Out',
+                        icon: Icons.logout,
+                        onPressed: () {},
+                      ),
+                    ]),
+                  ),
                 ),
               ]),
             ),
-          ),
-        ]),
-      )
+          ]),
+        )
+      ])
     ]));
   }
 }
