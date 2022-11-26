83853c8e3038694bea4a9bfcb119a504b3898ac0
Rohit Balage <thatcomputerboy26@gmail.com>
2022-11-25 19:58:38 -0500

    overwrite store dart file

--- lib/gallaries/beauty_gallery.dart
+++ lib/gallaries/beauty_gallery.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
