13a37fa645d9a99e38cded169a2aad33f9188271
Rohit Balage <thatcomputerboy26@gmail.com>
2022-11-25 20:45:25 -0500

    replace cover image and edit store info. is established

--- lib/minor_screens/edit_Store.dart
+++ lib/minor_screens/edit_Store.dart
@@ -16,6 +16,7 @@ class EditStore extends StatefulWidget {
 class _EditStoreState extends State<EditStore> {
   final ImagePicker _picker = ImagePicker();
   XFile? _imageFileLogo;
+  XFile? _imageFileCover;
   dynamic _pickedImageError;
 
   pickStoreLogo() async {
@@ -27,6 +28,24 @@ class _EditStoreState extends State<EditStore> {
           imageQuality: 95);
       setState(() {
         _imageFileLogo = pickedStoreLogo;
+      });
+    } catch (e) {
+      setState(() {
+        _pickedImageError = e;
+      });
+      print(_pickedImageError);
+    }
+  }
+
+  pickCoverImage() async {
+    try {
+      final pickedCoverImage = await _picker.pickImage(
+          source: ImageSource.gallery,
+          maxHeight: 300,
+          maxWidth: 300,
+          imageQuality: 95);
+      setState(() {
+        _imageFileCover = pickedCoverImage;
       });
     } catch (e) {
       setState(() {
@@ -103,9 +122,117 @@ class _EditStoreState extends State<EditStore> {
                 ),
               )
             ],
+          ),
+          Column(
+            children: [
+              Text(
+                'Cover Image',
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.w600),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(widget.data['coverimage']),
+                    radius: 60,
+                  ),
+                  Column(
+                    children: [
+                      YellowButton(
+                          label: 'Change',
+                          onPressed: () {
+                            pickCoverImage();
+                          },
+                          width: 0.25),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _imageFileCover == null
+                          ? const SizedBox()
+                          : YellowButton(
+                              label: 'Reset',
+                              onPressed: () {
+                                setState(() {
+                                  _imageFileCover = null;
+                                });
+                              },
+                              width: 0.25),
+                    ],
+                  ),
+                  _imageFileCover == null
+                      ? const SizedBox()
+                      : CircleAvatar(
+                          backgroundImage:
+                              FileImage(File(_imageFileCover!.path)),
+                          radius: 60,
+                        ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16),
+                child: Divider(
+                  color: Colors.yellow,
+                  thickness: 2.5,
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              initialValue: widget.data['storename'],
+              decoration: textFormDecoration.copyWith(
+                  labelText: 'store name', hintText: 'Enter store name'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              initialValue: widget.data['phone'],
+              decoration: textFormDecoration.copyWith(
+                  labelText: 'phone', hintText: 'Enter phone'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                YellowButton(
+                  label: 'cancle',
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  width: 0.25,
+                ),
+                YellowButton(
+                  label: 'save changes',
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  width: 0.5,
+                )
+              ],
+            ),
           )
         ],
       ),
     );
   }
 }
+
+var textFormDecoration = InputDecoration(
+  labelText: 'price',
+  hintText: 'price .. \$',
+  labelStyle: const TextStyle(color: Colors.purple),
+  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+  enabledBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.yellow, width: 1),
+      borderRadius: BorderRadius.circular(10)),
+  focusedBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.blueAccent, width: 2),
+      borderRadius: BorderRadius.circular(10)),
+);
